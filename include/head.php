6d77c24b3ac758bc08d56f056870e10e35b52355
Lav000 <77846416+Lav000@users.noreply.github.com>
2024-02-01 17:28:54 +0100

    update to php v1.0

---
+++ include/head.php
@@ -0,0 +1,14 @@
+<?php
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gmail</title>
+    <meta name="description" content="Description de la page">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/nav.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
+    </head>
+?>
